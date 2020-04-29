34d77d2b48a393aa1371cefe7a59d15901fba398
miyabilab <miyabi@miyabilab.net>
2020-04-29 22:48:01 +0900

    section10-127

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,64 @@
     <keep-alive>
       <component :is="currentComponent"></component>
     </keep-alive>
+    <div style="padding: 10rem;">
+      <h2>イベントのフォーム</h2>
+      <label for="title">タイトル</label>
+      <input type="text"
+          id="title"
+          v-model.lazy="eventData.title">
+      <p>{{ eventData.title }}</p>
+      <label for="maxNumber">最大人数</label>
+      <input type="number"
+          id="maxNumber"
+          v-model="eventData.maxNumber">
+      <p>{{ typeof eventData.maxNumber }}</p>
+      <label for="host">主催者</label>
+      <input type="text"
+          id="host"
+          v-model.trim="eventData.host">
+      <p>{{ eventData.host }}</p>
+      <lavel for="detail">イベントの内容</lavel>
+      <textarea id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
+      <p style="white-space: pre;">{{eventData.detail}}</p>
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+      <label for="isPrivate">非公開</label>
+      <p>{{eventData.isPrivate}}</p>
+      <p>参加条件</p>
+      <input type="checkbox"
+      id="10"
+      value="10代"
+      v-model="eventData.target">
+      <lavel for="10">10代</lavel>
+      <input type="checkbox"
+      id="20"
+      value="20代"
+      v-model="eventData.target">
+      <lavel for="30">20代</lavel>
+      <input type="checkbox"
+      id="30"
+      value="30代"
+      v-model="eventData.target">
+      <lavel for="10">30代</lavel>
+      <p>{{eventData.target}}</p>
+      <p>参加費</p>
+      <input type="radio"
+      id="free"
+      value="無料"
+      v-model="eventData.price">
+      <lavel for="free">無料</lavel>
+      <input type="radio"
+      id="paid"
+      value="有料"
+      v-model="eventData.price">
+      <lavel for="paid">有料</lavel>
+      <p>{{eventData.price}}</p>
+      <p>開催場所</p>
+      <select v-model="eventData.location" multiple>
+        <option v-for="location in locations" :key="location">{{location}}</option>>
+      </select>
+      <p>{{eventData.location}}</p>
+    </div>
   </div>
 </template>
 
@@ -21,7 +79,18 @@ export default {
   data() {
     return {
       number: 14,
-      currentComponent: 'Home'
+      currentComponent: 'Home',
+      locations: ["東京", "大阪", "名古屋"],
+      eventData: {
+        title: "タイトル",
+        maxNumber: 0,
+        host: "",
+        detail: "",
+        isPrivate: false,
+        target: [],
+        price: "無料",
+        location: [],
+      }
     };
   },
   components: {
