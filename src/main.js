f60a727a0ad577f00a7302f8783f90c4a7edd856
miyabilab <miyabi@miyabilab.net>
2020-04-29 23:44:47 +0900

    section10-139

--- src/main.js
+++ src/main.js
@@ -4,22 +4,16 @@ import LikeNumber from './components/LikeNumber';
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
-Vue.directive("border", {
-  bind(el, binding, vnode) {
-    // ディレクティブが初めて対象のデータに紐付いた時
-  },
-  inserted(el, binding, vnode) {
-    // 親Nodeに挿入された時
-  },
-  update(el, binding, vnode, oldVnode) {
-    // コンポーネントが変更される度。子コンポーネントが更新される時
-  },
-  componentUpdated(el, binding, vnode, oldVnode) {
-    // コンポーネントが更新される度。子コンポーネントが更新された時
-  },
-  unbinde(el, binding, vnode) {
-    // ディレクティブが紐付いている要素から取り除かれた時
-  },
+Vue.directive("border", function(el, binding) {
+  el.style.borderWidth = binding.value.width;
+  el.style.borderColor = binding.value.color;
+  el.style.borderStyle = binding.arg;
+  if (binding.modifiers.round) {
+    el.style.borderRadius = "0.5rem";
+  }
+  if (binding.modifiers.shadow) {
+    el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)";
+  }
 });
 
 new Vue({
