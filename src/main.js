64cb64e4271eae8f0525640376c6ef2746d2f98f
miyabilab <miyabi@miyabilab.net>
2020-04-30 21:19:41 +0900

    section12-157

--- src/main.js
+++ src/main.js
@@ -1,18 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import LikeNumber from './components/LikeNumber';
-
-Vue.config.productionTip = false
-Vue.component('LikeNumber', LikeNumber);
-Vue.filter("upperCase", function(value) {
-  console.log("filter")
-  return value.toUpperCase();
-});
-Vue.mixin({
-  created() {
-    console.log("global mixin");
-  }
-});
 
 new Vue({
   render: h => h(App),
