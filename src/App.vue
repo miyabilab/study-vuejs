c5c946171a40d75306ff33de107d1593e4115d8e
miyabilab <miyabi@miyabilab.net>
2020-05-04 16:10:42 +0900

    section12-174

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     <button @click="show = !show">切り替え</button>
     <br><br>
     <transition 
+      :css="false"
       @before-enter="beforeEnter"
       @enter="enter"
       @after-enter="afterEnter"
@@ -62,28 +63,47 @@ export default {
     };
   },
   methods: {
-    beforeEnter(el) {
+    beforeEnter(el) { // eslint-disable-line
       // 現れる前
+      el.style.transform = 'scale(0)'
     },
     enter(el, done) {
       // 現れる時
+      let scale = 0;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale += 0.1;
+        if (scale > 1) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
     },
-    afterEnter(el) {
+    afterEnter(el) { // eslint-disable-line
       // 現れた後
     },
-    enterCancelled(el) {
+    enterCancelled(el) { // eslint-disable-line
       // 現れるアニメーションがキャンセルされた時
     },
-    beforeLeave(el) {
+    beforeLeave(el) { // eslint-disable-line
       // 消える前
     },
     leave(el, done) {
       // 消える時
+      let scale = 1;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale -= 0.1;
+        if (scale < 0) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
     },
-    afterLeave(el) {
+    afterLeave(el) { // eslint-disable-line
       // 消えた後j
     },
-    leaveCancelled(el) {
+    leaveCancelled(el) { // eslint-disable-line
       // 消えるアニメーションがキャンセルされた時
     },
   }
