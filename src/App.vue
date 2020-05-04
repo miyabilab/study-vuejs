0a1b7c93a629fc1d89eaa1c397b039b0024f156b
miyabilab <miyabi@miyabilab.net>
2020-05-04 16:58:11 +0900

    section12-177

--- src/App.vue
+++ src/App.vue
@@ -144,13 +144,16 @@ export default {
   background-color: deeppink;
 }
 
+.fade-move {
+  transition: transform 1s;
+}
 .fade-enter {
   /*  現れる時の最初の状態 */
   opacity: 0;
 }
 .fade-enter-active {
   /* 現れる時のトランジションの状態 */
-  transition: opacity 0.5s;
+  transition: opacity 1s;
 }
 .fade-enter-to {
   /* 現れる時の最後の状態 */
@@ -162,7 +165,9 @@ export default {
 }
 .fade-leave-active {
   /* 消える時のトランジションの状態 */
-  transition: opacity 0.5s;
+  transition: opacity 1s;
+  position: absolute;
+  width: 200px;
 }
 .fade-leave-to {
   /* 消える時の最後の状態 */
