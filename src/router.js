15f5471018e1e28e45d2762651810981105fcc11
miyabilab <miyabi@miyabilab.net>
2020-05-05 14:35:52 +0900

    section13-193

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import Users from './views/Users';
+import Users from './views/Users.vue';
+import UsersPosts from './views/UsersPosts.vue';
+import UsersProfile from './views/UsersProfile.vue';
 
 Vue.use(Router);
 
@@ -9,6 +11,13 @@ export default new Router({
     mode: "history",
     routes: [
         {path: '/', component: Home},
-        {path: '/users/:id', component: Users, props: true },
+        {path: '/users/:id',
+         component: Users,
+         props: true,
+         children: [
+             { path: "posts", component: UsersPosts},
+             { path: "profile", component: UsersProfile, name: "users-id-profile"},
+         ]
+        },
     ]
 });
