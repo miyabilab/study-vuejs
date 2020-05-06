cbbc5af5188c8d5363d1d78304a839c339d597dd
miyabilab <miyabi@miyabilab.net>
2020-05-06 11:56:25 +0900

    section13-197

--- src/router.js
+++ src/router.js
@@ -22,11 +22,18 @@ export default new Router({
              default: Users,
              header: HeaderUsers,
          },
-         props: true,
+         props: {
+             default: true,
+             header: false,
+         },
          children: [
              { path: "posts", component: UsersPosts},
              { path: "profile", component: UsersProfile, name: "users-id-profile"},
          ]
         },
+        {
+            path: '/*',
+            redirect:"/" 
+        }
     ]
 });
