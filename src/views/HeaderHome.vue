b7eed2a168a3f24403070ad14b90e01990c70439
miyabilab <miyabi@miyabilab.net>
2020-05-06 22:31:34 +0900

    section13-204

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -3,7 +3,7 @@
     <nav>
       <h2>Home</h2>
       <router-link to="/" active-class="link--active" exact class="link">Home</router-link>
-      <router-link :to="'/users/' + (Number(id) + 1) + '/profile?#next-user'" active-class="link--active" exact class="link">Users</router-link>
+      <router-link :to="'/users/' + 1 + '/profile?#next-user'" active-class="link--active" exact class="link">Users</router-link>
     </nav>
   </div>
 </template>
