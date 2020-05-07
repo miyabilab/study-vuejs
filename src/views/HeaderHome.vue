9a838ebccd5e433b822a6ed522141a551c1fc2cd
miyabilab <miyabi@miyabilab.net>
2020-05-07 21:51:30 +0900

    section14-215

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -4,18 +4,21 @@
       <h2>Home</h2>
       <router-link to="/" active-class="link--active" exact class="link">Home</router-link>
       <router-link :to="'/users/' + 1 + '/profile?#next-user'" active-class="link--active" exact class="link">Users</router-link>
-      <button @click="increment(2)">+1</button>
-      <button @click="decrement(2)">-1</button>
+      <button @click="increment">+1</button>
+      <button @click="decrement">-1</button>
     </nav>
   </div>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
-
 export default {
   methods: {
-    ...mapMutations(["increment", "decrement"]),
+    increment() {
+      this.$store.dispatch('increment', 2);
+    },
+    decrement() {
+      this.$store.dispatch('decrement', 2);
+    }
   },
 }
 </script>
